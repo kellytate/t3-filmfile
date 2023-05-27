35fe9d87f41bb91a50caefede765f0ec1edf6fcc
kelly <kelly.tate.dev@gmail.com>
2023-05-27 08:35:41 -0700

    Sets initial textArea height and increases with input.

--- src/components/NewPostForm.tsx
+++ src/components/NewPostForm.tsx
@@ -1,17 +1,47 @@
 import { useSession } from "next-auth/react";
+import { useLayoutEffect, useRef, useState, useCallback } from "react";
 import { Button } from "./Button"
 import { ProfileImage } from "./ProfileImage";
 
-export function NewPostForm() {
-    const session = useSession()
+function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
+    if (textArea == null) return
+    textArea.style.height = "0"
+    textArea.style.height = `${textArea.scrollHeight}px`
+}
 
-    if (session.status !== "authenticated") return 
+export function NewPostForm() {
+    const session = useSession();
+    if (session.status !== "authenticated") return;
+    
+    return <Form />
+}
+
+function Form() {
+    const session = useSession();
+    const [inputValue, setInputValue] = useState("");
+    const textAreaRef = useRef<HTMLTextAreaElement>();
+    const inputRef = useCallback((textArea: HTMLTextAreaElement) => 
+    {
+        updateTextAreaSize(textArea);
+        textAreaRef.current = textArea
+    }, [])
+
+    useLayoutEffect(() => {
+        updateTextAreaSize(textAreaRef.current);
+    }, [inputValue]);
+
+    if (session.status !== "authenticated") return null;
     
     return (
         <form className="flex flex-col gap-2 border-b px-4 py-2">
             <div className="flex gap-4">
             <ProfileImage src={session.data.user.image} />
-            <textarea className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none" placeholder="something to share?"/>
+            <textarea
+            ref={inputRef}
+            style={{ height: 0 }}
+            value={inputValue}
+            onChange={e => setInputValue(e.target.value)}
+            className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none" placeholder="something to share?"/>
         </div>
         <Button className="self-end">Post</Button>
         </form>
