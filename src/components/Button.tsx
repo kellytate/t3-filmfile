c7cfdaa8d4dea6f7b11439eb12596e993f374d16
kelly <kelly.tate.dev@gmail.com>
2023-05-26 23:13:32 -0700

    Modifies Button component.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,6 +6,13 @@ type ButtonProps = {
     className?: string
 } & DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
 
-export function Button({ small = false, gray = false, className="", ...props }: ButtonProps) {
-    return <button{...props}></button>
+export function Button({
+    small = false, 
+    gray = false, 
+    className="", 
+    ...props
+}: ButtonProps) {
+    const sizeClasses = small ? "px-2 py-1" : "px-4 py-2 font-bold"
+    // const colorClasses = gray ? "bg-gray-400 hover: bg-gray-300 focus-visible:bg-gray-300" :
+    return <button{...props}></button>;
 }
