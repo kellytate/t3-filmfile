25c3d492d1061695f048e8e799065c63b5d9f7d0
kelly <kelly.tate.dev@gmail.com>
2023-05-27 13:56:58 -0700

    Adds Tabs to feed.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,37 @@
 import { type NextPage } from "next";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 import { InfinitePostList } from "~/components/InfinitePostList";
 import { NewPostForm } from "~/components/NewPostForm";
 import { api } from "~/utils/api";
 
+const TABS = ["Recent", "Following"] as const
 
 const Home: NextPage = () => {
-
+  const [selectedTab, setSelectedTab] = useState<(typeof TABS)[number]>("Recent")
+  const session = useSession()
   return <>
   <header className="sticky top-0 z-10 border-b bg-white pt-2">
     <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
+    {session.status === "authenticated" && (
+      <div className="flex">
+        {TABS.map((tab) => {
+          return (
+            <button 
+              key={tab} 
+              className={`flex-grow p-2 hover:bg-gray-200 focus-visible:bg-gray-200 ${
+                tab === selectedTab 
+                  ? "border-b-4 border-b-blue-500 font-bold"
+                  : ""
+              }`}
+              onClick={() => setSelectedTab(tab)}
+            >
+              {tab}
+            </button>
+          );
+        })}
+      </div>
+    )}  
   </header>
   <NewPostForm />
   <RecentPosts />
