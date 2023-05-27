9e175dfa8ef6fbe4b6446c587d2fc1cd9e1e3b3e
kelly <kelly.tate.dev@gmail.com>
2023-05-27 07:48:06 -0700

    Creates ProfileImage component.

--- src/components/NewPostForm.tsx
+++ src/components/NewPostForm.tsx
@@ -1,10 +1,16 @@
+import { useSession } from "next-auth/react";
 import { Button } from "./Button"
+import { ProfileImage } from "./ProfileImage";
 
 export function NewPostForm() {
+    const session = useSession()
+
+    if (session.status !== "authenticated") return 
+    
     return (
         <form className="flex flex-col gap-2 border-b px-4 py-2">
             <div className="flex gap-4">
-            {/* <ProfileImage src="#url" /> */}
+            <ProfileImage src={session.data.user.image} />
             <textarea className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none" placeholder="something to share?"/>
         </div>
         <Button className="self-end">Post</Button>
