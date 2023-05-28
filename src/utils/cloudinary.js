343e39eb28708682c593945d9721b94f5cdb8fa4
kelly <kelly.tate.dev@gmail.com>
2023-05-28 10:58:21 -0700

    Adds next-cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 // const cloudinary = require('./cloudinary').v2;
-import * as cloudinary from 'cloudinary-react'
+import * as cloudinary from 'next-cloudinary';
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
