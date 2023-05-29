6a7495d2b6f3e0f98a33c039e7a54eebf80a71b1
kelly <kelly.tate.dev@gmail.com>
2023-05-29 09:10:24 -0700

    Adjusts padding and button styles.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,12 +12,12 @@ export function Button({
     className="", 
     ...props
 }: ButtonProps) {
-    const sizeClasses = small ? "px-2 py-1" : "px-4 py-2 font-bold"
+    const sizeClasses = small ? "px-2 py-1" : "px-4 py-2"
     const colorClasses = gray 
-        ? "bg-zinc-400 hover: bg-gray-300 focus-visible:bg-gray-300"
-        : "bg-zinc-950 hover: bg-blue-400 focus-visible:bg-blue-400";
+        ? "bg-zinc-400 hover:bg-gray-300 focus-visible:bg-gray-300"
+        : "hover:border-stone-400";
 
     return <button className={` border border-stone-800 transition-colors duration-200 
-    disabled:cursor-not-allowed disabled:opacity-50 text-white ${sizeClasses} 
+    disabled:cursor-not-allowed disabled:opacity-50 dark:text-white ${sizeClasses} 
     ${colorClasses} ${className}`} {...props}></button>;
 }
